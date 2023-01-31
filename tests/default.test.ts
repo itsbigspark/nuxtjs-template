8e4d929915effcb00e39f1bd49ad22f69e73e6ee
Joseph Maclean Arhin <josephmacleanarhin@outlook.com>
2023-01-31 16:51:46 +0000

    chore: add vitest for tests and some linters

---
+++ tests/default.test.ts
@@ -0,0 +1,7 @@
+import { mount } from '@vue/test-utils'
+import Hello from '../components/Hello.vue'
+import { expect } from 'vitest'
+
+test('mount component', async () => {
+  expect(true).toBeTruthy()
+})
