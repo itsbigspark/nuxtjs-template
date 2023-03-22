27035d21b2905c6da117efee47e4182886f4dbc9
Joseph Maclean Arhin <josephmacleanarhin@outlook.com>
2023-03-22 13:44:24 +0000

    chore: add nuxt devtools

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,7 @@ export default defineNuxtConfig({
     modules: [
         '@nuxtjs/tailwindcss',
         '@pinia/nuxt',
+        '@nuxt/devtools',
     ],
     typescript: {
         strict: true,
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,11 @@
 <template>
-  <p class="text-lg">
-    Hello world
+  <p class="text-lg font-bold">
+    {{ data.name }}
   </p>
   <my-component />
 </template>
+<script setup lang="ts">
+const data = reactive({
+  name: "hello there"
+})
+</script>
